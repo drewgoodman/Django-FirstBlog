e1a5be530f8cc598debb3d7144234d4290ebf351
rdogm13 <drg6525@yahoo.com>
2020-04-29 20:16:25 -0700

    detail query per blog entry added

--- posts/urls.py
+++ posts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('<int:post_id>', views.post_home),
     path('', views.post_list),
     path('create/', views.post_create),
-    path('detail/', views.post_detail),
+    path('<int:id>/', views.post_detail),
     path('update/', views.post_update),
     path('delete/', views.post_delete),
 
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+
 # Create your views here.
+from .models import Post
+
 # Function based views vs class based views
 
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
-    return HttpResponse("<h1>Detail/h1>")
+def post_detail(request, id=None):
+    # instance = Post.objects.get(id=1)
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title" : instance.title,
+        "instance" : instance
+    }
+    return render(request, "post_detail.html", context)
 
 def post_list(request):
-    return render(request, "index.html", {})
+    queryset = Post.objects.all()
+    context = {
+        "object_list" : queryset,
+        "title" : "List"
+    }
+    return render(request, "index.html", context)
+
+    # if request.user.is_authenticated:
+    #     context = {
+    #         "title" : "My User List"
+    #     }
+    #     return render(request, "index.html", context)
+    # else:        
+    #     context = {
+    #         "title" : "List"
+    #     }
+    #     return render(request, "index.html", context)
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
