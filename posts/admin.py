1649c59a8f18f358575c733af329bc6a72571a5a
rdogm13 <drg6525@yahoo.com>
2020-04-29 18:26:20 -0700

    added blog post model and admin view

---
+++ posts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+# from posts.models import Post
+from .models import Post #relative import
+
+class PostModelAdmin(admin.ModelAdmin):
+
+    list_display = ["title", "timestamp", "updated"]
+    list_display_links = ["updated"] #change which field has a link to change the entry
+    list_editable = ["title"]
+
+    list_filter = ["updated", "timestamp"] #create options to sort by these fields
+    search_fields= ["title","content"] #allow for searching in these fields
+
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
+
