81c718fb6a37349db39fa8f436ac6b2f888d0087
rdogm13 <drg6525@yahoo.com>
2020-05-06 01:31:44 -0700

    reply function added with child/parent relationship

--- comments/models.py
+++ comments/models.py
@@ -6,10 +6,14 @@ from django.db import models
 
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
-        queryset = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id)
+        queryset = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
         return queryset
 
 
@@ -20,10 +24,24 @@ class Comment(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
 
+    parent      = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+
     content     = models.TextField()
     timestamp   = models.DateTimeField(auto_now_add=True)
 
     objects = CommentManager()
 
+    class Meta:
+        ordering = ['-timestamp']
+
     def __str__(self):
         return str(self.user.username)
+
+    def children(self): #replies
+        return Comment.objects.filter(parent=self).order_by('timestamp')
+    
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
