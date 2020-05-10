88de0ec02971ee993686238aee6d4c2a2e7f6581
rdogm13 <drg6525@yahoo.com>
2020-05-10 12:38:01 -0700

    static cdn folder added / compression setting temporarily disabled

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
