8dfc3624f2815329cbf0d2de6d76656faf666c82
rdogm13 <drg6525@yahoo.com>
2020-05-18 17:08:43 -0700

    sidebar building / minor post list tweaks

--- posts/processors.py
+++ posts/processors.py
@@ -3,10 +3,30 @@ from django.template import RequestContext, Template
 from .models import Post
 
 def sidebar_context(request):
-    
-    recent_posts = Post.objects.active()[:4]
+
+    qs_posts = Post.objects.active()
     tag_links = Post.tags.most_common()[:6]
+    recent_posts = qs_posts[:4]
+
+    # archive_list = []
+
+    # for post in qs_posts:
+    #     date = "%s-%s" % (post.publish.month,post.publish.year)
+    #         if date in archive_list.values():
+    #             archive_list['count'] += 1
+    #             break
+    #         else:
+    #             archive_month = {
+    #                 "date": date,
+    #                 "month": post.publish.month,
+    #                 "year": post.publish.year,
+    #                 "count": count
+    #             }
+    #             archive_list.append(archive_month)
+    # print(archive_list)
+
     return {
         'recent_posts': recent_posts,
         'tag_links': tag_links,
+        # 'archive_list': archive_list,
         }
