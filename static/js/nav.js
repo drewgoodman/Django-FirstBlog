0a00f66a156077ec4c3ac3ad9ee7237e39d6d7a8
rdogm13 <drg6525@yahoo.com>
2020-05-20 00:50:39 -0700

    created nav.js / imported FontAwesome / created simple scroll to top component

---
+++ static/js/nav.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+
+    $(".search-bar-form").on("submit", function (event) {
+        event.preventDefault();
+        var search_query = $(this).find(".search-bar-query").val();
+        var search_href = search_query ? home_url + "?query=" + search_query : home_url;
+        window.location.replace(search_href);
+
+    })
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > $(window).height()) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    })  
+
+    $('#scroll-to-top').click(function() {
+        $('html, body').animate({scrollTop: 0}, 700);
+        return false;
+    })
+
+});
--- static/js/posts.js
+++ static/js/posts.js
@@ -1,64 +1,57 @@
-    
-    $(document).ready(function() {
 
-        $(".search-bar-form").on("submit",function(event) {
-            event.preventDefault();
-            var search_query = $(this).find(".search-bar-query").val();
-            var search_href = search_query ? home_url + "?query=" + search_query : home_url;
-            window.location.replace(search_href);
+$(document).ready(function () {
 
-        })
 
-        $(".comment-reply-btn").click(function(event){
-            event.preventDefault();
-            $(this).parent().next(".comment-reply").fadeToggle();
-        })
- 
-        $(".content-markdown").each(function(){
-            var content = $(this).text();
-            var markedContent = marked(content);
-            $(this).html(markedContent)
-        })
+    $(".comment-reply-btn").click(function (event) {
+        event.preventDefault();
+        $(this).parent().next(".comment-reply").fadeToggle();
+    })
 
-        $(".post-detail-item img").each(function() {
+    $(".content-markdown").each(function () {
+        var content = $(this).text();
+        var markedContent = marked(content);
+        $(this).html(markedContent)
+    })
+
+    $(".post-detail-item img").each(function () {
+        $(this).addClass("img-responsive");
+    })
+
+    var titleInput = $("#id_title");
+
+    function setTitle(value) {
+        $("#preview-title").text(value);
+    }
+
+    setTitle(titleInput.val())
+
+    titleInput.keyup(function () {
+        var newTitle = $(this).val();
+        setTitle(newTitle);
+    })
+
+    var contentInput = $("#wmd-input-id_content");
+
+    function setContent(value) {
+        var markedContent = marked(value);
+        $("#preview-content").html(markedContent);
+        $("#preview-content img").each(function () {
             $(this).addClass("img-responsive");
         })
+    }
 
-        var titleInput = $("#id_title");
+    setContent(contentInput.val())
 
-        function setTitle(value) {        
-            $("#preview-title").text(value);
-        }
-
-        setTitle(titleInput.val())
-
-        titleInput.keyup(function() {
-            var newTitle = $(this).val();
-            setTitle(newTitle);
-        })
-
-        var contentInput = $("#wmd-input-id_content");
-
-        function setContent(value){
-            var markedContent = marked(value);
-            $("#preview-content").html(markedContent);
-            $("#preview-content img").each(function() {
-                $(this).addClass("img-responsive");
-            })
-        }
-
-        setContent(contentInput.val())
-
-        contentInput.keyup(function() {
-            var newContent = $(this).val();
-                setContent(newContent);
-        })
-
-        $("#id_tags").attr('data-role', 'tagsinput');
-
-        // var imageUploader = $("#id_image");
-        // imageUploader.attr('name','file');
-        // imageUploader.addClass('dropzone')
-        // $("#id_image").dropzone({ url: "/"});
-
+    contentInput.keyup(function () {
+        var newContent = $(this).val();
+        setContent(newContent);
     })
+
+    $("#id_tags").attr('data-role', 'tagsinput');
+
+    // var imageUploader = $("#id_image");
+    // imageUploader.attr('name','file');
+    // imageUploader.addClass('dropzone')
+    // $("#id_image").dropzone({ url: "/"});
+
+})
