5472f31ef3353fa74dd9b3ce9367fb9843789736
rdogm13 <drg6525@yahoo.com>
2020-05-22 18:22:22 -0700

    basic category model

--- posts/models.py
+++ posts/models.py
@@ -16,6 +16,27 @@ from comments.models import Comment
 from .utils import get_read_time
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def get_posts(self):
+        instance = self
+        qs = Post.objects.filter_by_instance(instance)
+        return qs
+
+    @property
+    def get_post_count(self):
+        count = 0
+        instance = self
+        count = len(Post.objects.filter_by_instance(instance))
+        return count
+
+
+
 class PostManager(models.Manager):
     #Post.objects.all() = super(postManager, self).all()
     def active(self, *args, **kwargs):
