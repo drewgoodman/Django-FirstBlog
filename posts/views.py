f0d57294b44795da6cabd62c6a1f0627b2f634ea
rdogm13 <drg6525@yahoo.com>
2020-05-24 13:10:16 -0700

    class functions to update page title with filter type and search results

--- posts/views.py
+++ posts/views.py
@@ -114,14 +114,15 @@ class PostListView(View):
     
     title = "Post List"
 
+    def update_title(self, queryset, title_base, filter_name):
+        self.title = "%s: %s -- (%s results found)" % (title_base, filter_name, len(queryset))
+
     def paginate_list(self, queryset, page_number, max_posts):
         paginator = Paginator(queryset, max_posts)
         return paginator.get_page(page_number)
 
     def filter_by_slug(self, queryset_list, slug):
-        tag = get_object_or_404(Tag, slug=slug)
-        filtered_queryset = queryset_list.filter(tags=tag)
-        return filtered_queryset
+        pass
 
     def filter_by_query(self, queryset_list, query):
         filtered_queryset = queryset_list.filter(
@@ -148,6 +149,7 @@ class PostListView(View):
             query = request.GET.get("query")
             if query:
                 queryset_list = self.filter_by_query(queryset_list, query)
+                self.update_title(queryset_list,"Search results for",query)
 
         page_number = request.GET.get('page')
         page_obj = self.paginate_list(queryset_list, page_number, 10)
@@ -162,12 +164,19 @@ class PostListView(View):
 class PostTagView(PostListView):
     title = "Filter By Tag: " 
 
+    def filter_by_slug(self, queryset_list, slug):
+        tag = get_object_or_404(Tag, slug=slug)
+        filtered_queryset = queryset_list.filter(tags=tag)
+        self.update_title(filtered_queryset,"Tag",tag)
+        return filtered_queryset
+
 class PostCategoryView(PostListView):
     title = "Filter By Category: "
 
     def filter_by_slug(self, queryset_list, slug):
         category = get_object_or_404(Category, slug=slug)
         filtered_queryset = queryset_list.filter(category=category)
+        self.update_title(filtered_queryset,"Category",category.name)
         return filtered_queryset
 
 
